2ee3676ae632221a965270cbdb91c325257e7eca
pafmon <pafmon@gmail.com>
2022-01-10 11:37:05 +0100

    updated test template

---
+++ generate.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const mustache = require('mustache');
+
+const atFile = "./templates/admin";
+const utFile = "./templates/user";
+const rootTestFolder = "./tests" 
+
+var session = "simulacro2021";
+var codes = [543650,815247,104598];
+var token = "melon";
+
+const testFolder = rootTestFolder+"/"+session;
+
+if (!fs.existsSync(testFolder)){
+    fs.mkdirSync(testFolder);
+}else{
+    console.error(`Output test directory <${testFolder}> already exists.`);
+    process.exit(1);
+}
+
+var adminTemplate = null;
+try {
+  adminTemplate = fs.readFileSync(atFile, 'utf8')
+} catch (err) {
+  console.error(`Error reading admin template <${atFile}>:`+err);
+  process.exit(1);
+}
+
+var userTemplate = null;
+try {
+  userTemplate = fs.readFileSync(utFile, 'utf8')
+} catch (err) {
+  console.error(`Error reading user template <${utFile}>:`+err);
+  process.exit(1);
+}
+
+const adminTestFile = testFolder+"/admin.spec.js";
+const adminTest = mustache.render(adminTemplate, {session});
+
+try {
+    fs.writeFileSync(adminTestFile, adminTest)
+} catch (err) {
+    console.error(`Error writing admin test <${adminTestFile}>:`+err);
+    process.exit(1);
+}
+
+codes.forEach( (code) =>{
+
+    const userTestFile = testFolder+"/"+code+".spec.js";
+    const userTest = mustache.render(userTemplate, {code,token});
+    
+    try {
+        fs.writeFileSync(userTestFile, userTest)
+    } catch (err) {
+        console.error(`Error writing user test <${userTestFile}>:`+err);
+        process.exit(1);
+    }
+});
+
+console.log(`Test with ${codes.length} users for session <${session}> generated at:\n  <${testFolder}>`);
+console.log("\nRun it with the following command:");
+console.log(`npx playwright test ${testFolder}`);
+
